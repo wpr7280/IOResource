2c730ce3fe00a5a86bd15f944b6230d82d59f8a6
wpr7280 <617072887@qq.com>
2015-11-09 22:04:16 +0800

    this a project to study spring resource

---
+++ src/com/wpr/io/InputStreamSource.java
@@ -0,0 +1,25 @@
+package com.wpr.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * InputStreamSource封装任何能够返回InputStream的类
+ * <ul>包括：
+ * 		<li>File类</li>
+ * 		<li>classpath下的资源</li>
+ * 		<li>Byte Array等</li>
+ * </ul>
+ * 这个类也是资源抽象类Resource {@link Resource}的父类
+ * @author wpr
+ * @since 11.09.2015
+ * @see Resource
+ */
+public interface InputStreamSource {
+	/**
+	 * 用来返回一个输入流
+	 * @return 底层资源的InputStream （must not be {@code null}）
+	 * @throws IOException	if stream could not be opened
+	 */
+	InputStream getInputStrean() throws IOException;
+}
---
+++ src/com/wpr/io/Resource.java
@@ -0,0 +1,9 @@
+package com.wpr.io;
+/**
+ * 
+ * @author wpr
+ *
+ */
+public interface Resource extends InputStreamSource{
+	
+}	
