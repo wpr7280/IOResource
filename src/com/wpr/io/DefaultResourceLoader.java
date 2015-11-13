56d56ba22691beb9523a71020177ee4e92a29d5c
wpr7280 <617072887@qq.com>
2015-11-13 19:53:12 +0800

    修改了一些细节

--- src/com/wpr/io/DefaultResourceLoader.java
+++ src/com/wpr/io/DefaultResourceLoader.java
@@ -1,4 +1,9 @@
 package com.wpr.io;
+
+import java.net.URL;
+
+import com.wpr.utils.ClassUtils;
+
 /**
  * <p>ResourceLoader默认实现类
  * <p>如果location是一个URL形式的，将会返回{@link UrlResource},
@@ -11,21 +16,14 @@ public class DefaultResourceLoader implements ResourceLoader {
 	private ClassLoader classLoader;
 	
 	public DefaultResourceLoader() {
-		this.classLoader = DefaultResourceLoader.getDefaultClassLoader();
-	}
-	public static ClassLoader getDefaultClassLoader(){
-		ClassLoader cl = Thread.currentThread().getContextClassLoader();
-		if(cl==null){
-			cl = DefaultResourceLoader.class.getClassLoader();
-		}
-		return cl;
+		this.classLoader = ClassUtils.getDefaultClassLoader();
 	}
 	public DefaultResourceLoader(ClassLoader classloader) {
 		this.classLoader = classloader;
 	}
 	/**
 	 *<p> 获得资源，首先判断是否ClassPath类型（classpath:前缀型）
-	 *否则判断为UrlResource，如果还不行，
+	 *否则判断为UrlResource，如果还不行，则认为是资源型文件（file）
 	 */
 	@Override
 	public Resource getResource(String location) {
@@ -33,19 +31,25 @@ public class DefaultResourceLoader implements ResourceLoader {
 			throw new IllegalArgumentException("Location must not be null");
 		}
 		if(location.startsWith(CLASSPATH_URL_PREFIX)){
-			//return new Class
+			return new ClassPathResource(location.substring(CLASSPATH_URL_PREFIX.length()),getClassLoader());
+		}else{
+			try {
+				URL  url = new URL(location);
+				return new UrlResource(url);
+			} catch (Exception e) {
+				
+			}
 		}
+		
 		return null;
 	}
 
 	public ClassLoader getClassLoader() {
-		return (this.classLoader!=null)?this.classLoader:DefaultResourceLoader.getDefaultClassLoader();
+		return (this.classLoader!=null)?this.classLoader:ClassUtils.getDefaultClassLoader();
 	}
 
 	public void setClassLoader(ClassLoader classLoader) {
 		this.classLoader = classLoader;
 	}
-
-
 
 }
